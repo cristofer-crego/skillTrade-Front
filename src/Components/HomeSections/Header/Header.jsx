89e246030e3c8b9177855584707aac3839da5d52
cristofer.crego <cregocrisoferlf@gmail.com>
2024-09-18 21:04:29 -0300

    agrego lupa en buscar

--- src/Components/HomeSections/Header/Header.jsx
+++ src/Components/HomeSections/Header/Header.jsx
@@ -64,7 +64,7 @@ const Header = () => {
           value={searchTerm} // Vínculo al valor del estado
           onChange={handleSearchChange}
         />
-        {/* <span className="icon">🔍</span> */}
+        <span className="icon">🔍</span>
       </div>
       <Selector />
     </header>
